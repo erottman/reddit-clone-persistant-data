08b5909173af770343bbd79f17de4836bc86fc36
Erica Rottman <erica7650@gmail.com>
2017-05-26 09:42:29 -0700

    changed repository including backend

---
+++ app/public/app.config.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular.module('reddit').config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+
+    // this line is optional
+    // $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '/',
+        component: 'app',
+      })
+  }
+
+}());
